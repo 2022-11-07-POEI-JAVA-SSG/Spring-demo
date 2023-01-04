fdd988ea0354362c274eaa00275cf49fd71b0b2e
jcdominguez <jcd.pro@gmail.com>
2023-01-04 10:03:46 +0100

    clean imports

--- src/test/java/com/example/demo/DemoApplicationUnitTests.java
+++ src/test/java/com/example/demo/DemoApplicationUnitTests.java
@@ -1,11 +1,6 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DemoApplicationUnitTests {
