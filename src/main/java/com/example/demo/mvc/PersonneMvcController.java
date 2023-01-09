65c4791fcfc54b1483366dc6e1e278333d3b5a3f
jcdominguez <jcd.pro@gmail.com>
2023-01-09 10:21:20 +0100

    Thymeleaf: Ajout d'un texte dynamique

--- src/main/java/com/example/demo/mvc/PersonneMvcController.java
+++ src/main/java/com/example/demo/mvc/PersonneMvcController.java
@@ -1,13 +1,17 @@
 package com.example.demo.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PersonneMvcController {
 
     @GetMapping("personnes")
-    public String affichePersonnes(){
+    public String affichePersonnes(Model model){
+
+        String prenom = "Jean-Christophe";
+        model.addAttribute("prenom", prenom);
 
         return "personnes.html";
     }
