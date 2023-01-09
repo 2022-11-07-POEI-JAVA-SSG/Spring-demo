7e58f4b3de328f4609148a6705648dcabd38a9ce
jcdominguez <jcd.pro@gmail.com>
2023-01-09 10:24:16 +0100

    Thymeleaf: Ajout d'un query param à afficher dans le HTML

--- src/main/java/com/example/demo/mvc/PersonneMvcController.java
+++ src/main/java/com/example/demo/mvc/PersonneMvcController.java
@@ -3,14 +3,14 @@ package com.example.demo.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PersonneMvcController {
 
     @GetMapping("personnes")
-    public String affichePersonnes(Model model){
+    public String affichePersonnes(Model model, @RequestParam String prenom){
 
-        String prenom = "Jean-Christophe";
         model.addAttribute("prenom", prenom);
 
         return "personnes.html";
